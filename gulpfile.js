83c6c9e05959fcb662fe45bd2388d22a48c7fe59
k.solovev <k.solovev.work@gmail.com>
2020-05-07 16:11:20 +0300

    add desktop version except catalog block

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require ('gulp');
-const sourcemap = require ('gulp-sourcemaps');
+// const sourcemap = require ('gulp-sourcemaps');
 const sass = require ('gulp-sass');
 const autoprefixer = require ('autoprefixer');
 const postcss = require ('gulp-postcss');
@@ -11,10 +11,10 @@ const browserSync = require ('browser-sync').create();
 gulp.task('css', function () {
   return gulp.src('src/scss/style.scss')
   .pipe(plumber())
-  .pipe(sourcemap.init())
+  // .pipe(sourcemap.init())
   .pipe(sass())
   .pipe(postcss([autoprefixer()]))
-  .pipe(sourcemap.write('.'))
+  // .pipe(sourcemap.write('.'))
   .pipe(gulp.dest('src/css'))
   .pipe(browserSync.stream())
 });
@@ -23,9 +23,9 @@ gulp.task('server', function () {
   browserSync.init({
     server: {
       baseDir: 'src/',
-      index: 'index.html',
-      notify: false
-    }
+      index: 'index.html'
+    },
+    notify: false
   })
 
   browserSync.watch('src/scss/**/*.scss', gulp.series('css'));
