c39c31a97071f6b8d1ca4c5915c0003c4029e967
k.solovev <k.solovev.work@gmail.com>
2020-05-09 22:58:34 +0300

    add catalog block all version

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ const overlay = document.querySelector('.overlay');
 const body = document.querySelector('.document-body');
 const popupWindow = document.querySelector('.popup-order');
 const popupCloseBtn = document.querySelector('.popup-order__close');
+const mouseIcon = document.querySelector('.promo__mouse');
+const mouseWheelIcon = document.querySelector('.promo__mouse svg path');
 
 // бургер
 burger.addEventListener('click', function(evt) {
@@ -14,20 +16,88 @@ burger.addEventListener('click', function(evt) {
   menu.classList.toggle('header__navigation--active');
 });
 
-//модальное окно
-orderBtn.addEventListener('click', function(evt) {
-  overlay.classList.remove('visually-hidden');
-  body.classList.add('scroll-hidden');
+$(document).ready(function() {
+  // модальное окно
+  // функция открытия модального окна
+  const openPopup = e => {
+    $('.overlay').removeClass('visually-hidden');
+    $('.document-body').addClass('scroll-hidden');
+    $('.main').addClass('blur');
+    $('.footer').addClass('blur');
+  }
 
-  popupCloseBtn.addEventListener('click', function(evt) {
-    overlay.classList.add('visually-hidden');
-    body.classList.remove('scroll-hidden');
+  // функция закрытия модального окна
+  const closePopup = function () {
+    $('.overlay').addClass('visually-hidden');
+    $('.document-body').removeClass('scroll-hidden');
+    $('.main').removeClass('blur');
+    $('.footer').removeClass('blur');
+  }
+
+  console.log(openPopup);
+
+
+
+  // открытие
+  $('.sales__btn').on('click', function(e) {
+    $('.overlay').removeClass('visually-hidden');
+    $('.document-body').addClass('scroll-hidden');
+    $('.main').addClass('blur');
+    $('.footer').addClass('blur');
+  
+    // закрытие на крестик
+    $('.popup-order__close').on('click', function(e) {
+      $('.overlay').addClass('visually-hidden');
+      $('.document-body').removeClass('scroll-hidden');
+      $('.main').removeClass('blur');
+      $('.footer').removeClass('blur');
+    })
+
+    // закрытие на клик вне окна
+    $('.document-body').on('mouseup', function(e) {
+      const popupWindow = $('.popup-order');
+      if (e.target != popupWindow[0] && popupWindow.has(e.target).length === 0) {
+        $('.overlay').addClass('visually-hidden');
+        $('.document-body').removeClass('scroll-hidden');
+        $('.main').removeClass('blur');
+        $('.footer').removeClass('blur');
+      }
+    })
+
+    // закрытие на ESC
+    $('.document-body').on('keyup', function(e) {
+      if (e.keyCode === 27) {
+        $('.overlay').addClass('visually-hidden');
+        $('.document-body').removeClass('scroll-hidden');
+        $('.main').removeClass('blur');
+        $('.footer').removeClass('blur');
+      }
+    })
+  })
+
+  // работа слайдера
+  $('.slider').slick({
+    mobileFirst: true,
+    arrows:false,
+    dots:true,
+    slidesToShow:1,
+
+    responsive:[
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow:2,
+          slidesToScroll:2
+        }
+      },
+      {
+        breakpoint: 1919,
+        settings: 'unslick'
+      }
+    ]
   });
-})
+});
 
-// body.addEventListener('click', function(evt) {
-//   if (evt.target != popupWindow) {
-//     overlay.classList.add('visually-hidden');
-//     body.classList.remove('scroll-hidden');
-//   }
-// });
+// анимация иконки мыши при наведении
+mouseIcon.addEventListener('mouseover', evt => mouseWheelIcon.setAttribute('d', 'M19 29.46484L19 18.9296'));
+mouseIcon.addEventListener('mouseout', evt => mouseWheelIcon.setAttribute('d', 'M19 9.46484L19 18.9296'));
